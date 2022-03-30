fbeeca7f564ffadf0bb7e86cf5b442debeae67ad
WillyRLV <williamlv1998@gmail.com>
2022-03-30 10:35:43 -0500

    proyecto limpio

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { Route , Routes, BrowserRouter } from 'react-router-dom';
-import Home from '../pages/Home';
-import Navbar from '../layout/NavBar';
-import Product from '../pages/Product';
-import Blog from '../pages/Blog';
 
 const Router = () => {
     return (
      <BrowserRouter>
 <Routes>
-    <Route  element={<Navbar/>}>
-    <Route path="/" element={<Home/>} /> 
-    <Route path="/product" element={<Product/>} />
-    <Route path='/blog' element={<Blog/>} />
-    {/*RUTA X */}
-    {/*AGregando ruta 2 */}
+{/**aqui irán las rutas */}
 
-    </Route>
 </Routes>
      </BrowserRouter>
     )
