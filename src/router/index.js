8141a28443f47b41dc709f19734e91f55f526b78
WillyRLV <williamlv1998@gmail.com>
2022-03-30 10:47:27 -0500

    agreango ultima parte

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route , Routes, BrowserRouter } from 'react-router-dom';
+import Product from '../pages/Product';
 
 const Router = () => {
     return (
      <BrowserRouter>
 <Routes>
 {/**aqui irán las rutas */}
-
+<Route path='/' element={<Product/>}/>
 </Routes>
      </BrowserRouter>
     )
