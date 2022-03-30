9e2696d0e48cfed2e73dcfd322678c000b4c6c1f
WillyRLV <williamlv1998@gmail.com>
2022-03-30 09:32:54 -0500

    proyecto limpio

---
+++ src/pages/Blog/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./blog.css"
+
+const Blog = () => {
+    return (
+        <div className='div-blog'>
+            <h4>Blog</h4>
+            <h5>EDITADO</h5>
+        </div>
+    );
+};
+
+export default Blog;
